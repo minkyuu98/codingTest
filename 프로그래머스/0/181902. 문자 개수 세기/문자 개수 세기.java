9d31ad9634b89469473517fdabcd110263ff70af
minkyuu98 <151510050+minkyuu98@users.noreply.github.com>
2024-07-08 01:26:06 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,18 @@
+
+
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+
+		
+		for(int i=0; i<my_string.length(); i++) {
+			if((int)my_string.charAt(i)<97) {
+				answer[(int)my_string.charAt(i)-65]++;
+			}else {
+				answer[(int)my_string.charAt(i)-71]++;
+			}
+		}
+        
+        return answer;
+    }
+}
