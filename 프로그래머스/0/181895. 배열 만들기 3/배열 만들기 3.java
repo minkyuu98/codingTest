585fdb677bf217d0787ec0766962e57a1ed2dec3
minkyuu98 <151510050+minkyuu98@users.noreply.github.com>
2024-07-09 14:42:18 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.51 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int[] arr, int[][] intervals) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        
+        for(int i=0; i<intervals.length; i++){
+            for(int j=intervals[i][0]; j<=intervals[i][1]; j++){
+                answer.add(arr[j]);
+            }
+        }
+        
+        
+        
+        
+        return answer;
+    }
+}
